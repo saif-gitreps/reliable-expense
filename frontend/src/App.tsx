4c55ff8ec8c8c9ffdd7a4a258772a1531657ba6b
Saif <droconos99@gmail.com>
2025-03-07 14:27:19 +0600

    Add: Pages boilerplate and common components.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-   return <div className="text-center text-8xl h-screen">hi</div>;
-}
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Transaction from "./pages/Transaction";
+import _404 from "./pages/_404";
 
+function App() {
+   const authUser = true;
+   return (
+      <>
+         {authUser && <Header />}
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/transaction/:id" element={<Transaction />} />
+            <Route path="*" element={<_404 />} />
+         </Routes>
+      </>
+   );
+}
 export default App;
---
+++ frontend/src/pages/_404.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function _404() {
+   return <div>_404</div>;
+}
+
+export default _404;
