8e467456eb0e7b56479547d11abc51d81a9fe70f
Saif <droconos99@gmail.com>
2025-03-09 01:37:37 +0600

    Add: Card component and transaction form.

--- frontend/src/pages/_404.tsx
+++ frontend/src/pages/_404.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function _404() {
-   return <div>_404</div>;
+   return (
+      <div>
+         <div className=" text-white">
+            <div className="flex h-screen">
+               <div className="m-auto text-center">
+                  <p className="text-sm md:text-base text-white p-2 mb-4">
+                     404 page not found
+                  </p>
+                  <Link to={"/"} className="text-white underline">
+                     Go back to home
+                  </Link>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default _404;
